636f4dc0d318472f37b8655f819599ea30dc9c5e
松井英俊 <dev.matsuihidetoshi@gmail.com>
2020-07-28 12:13:21 +0900

    created Note CRUD

---
+++ app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  module V1
+    class NotesController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      
+      before_action :set_note, only: [:show, :update, :destroy]
+
+      def index
+        notes = Note.order(created_at: :desc)
+        render json: { status: 'SUCCESS', message: 'Loaded notes', data: notes }
+      end
+
+      def show
+        note = Note.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Loaded the note', data: note }
+      end
+
+      def create
+        note = Note.new(note_params)
+        if note.save
+          render json: { status: 'SUCCESS', data: note }
+        else
+          render json: { status: 'ERROR', data: note.errors }
+        end
+      end
+
+      def destroy
+        @note.destroy
+        render json: { status: 'SUCCESS', message: 'Deleted the note', data: @note }
+      end
+
+      def update
+        if @note.update(note_params)
+          render json: { status: 'SUCCESS', message: 'Updated the note', data: @note }
+        else
+          render json: { status: 'SUCCESS', message: 'Not updated', data: @note.errors }
+        end
+      end
+
+      private
+
+      def set_note
+        @note = Note.find(params[:id])
+      end
+
+      def note_params
+        params.require(:note).permit(:title, :content, :user)
+      end
+    end
+  end
+end
