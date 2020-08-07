d445fc22281f312562a9801aee13d7c518207341
松井英俊 <dev.matsuihidetoshi@gmail.com>
2020-08-07 18:50:46 +0900

    レコード全長取得

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -8,7 +8,7 @@ module Api
 
       def index
         notes = Note.search(search_params).page(params[:page]).per(params[:per]).order(created_at: :desc)
-        render json: { status: 'SUCCESS', message: 'Loaded notes', data: notes }
+        render json: { status: 'SUCCESS', message: 'Loaded notes', data: notes, length: Note.count }
       end
 
       def show
